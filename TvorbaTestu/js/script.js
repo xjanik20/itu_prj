f9ee152dd128e778f564962fd805a2b796b03455
evens64 <kuba.hud@gmail.com>
2017-11-13 19:34:32 +0100

    focus

--- TvorbaTestu/js/script.js
+++ TvorbaTestu/js/script.js
@@ -1,4 +1,5 @@
 var id = 0;
+var focusId = -1;
 
 //vlozeni otazky
 function addDiv() {
@@ -7,15 +8,35 @@ function addDiv() {
     iDiv.id = id;
 
     iDiv.setAttribute("contenteditable", true);
-    iDiv.addEventListener('click', selectElement(id), false);
-    document.getElementById('col-main').appendChild(iDiv);
+    iDiv.onclick = selectElement;
+    //document.getElementById('1').appendChild(iDiv);
+    if (focusId == -1) {
+        iDiv.className += 'question';
+        document.getElementById('col-main').appendChild(iDiv);
+    }
+    else {
+        iDiv.className += 'answer';
+        document.getElementById(focusId).appendChild(iDiv);
+    }
+    
     id = id + 1;
 }
 
-function selectElement(id) {
-    var e = document.getElementById('id').event;
+function selectElement(e) {
     e.stopPropagation();
-    console.log("clicked");
+
+    console.log(e.type);
+
+    if (e.target.className == 'textarea') {
+        focusId = e.target.id;
+    }
+    else {
+        if (e.target.className != 'tool-image') {
+            focusId = -1;
+        }
+    }
+
+    console.log(focusId);
 }
 
 //nahradit TEXT
