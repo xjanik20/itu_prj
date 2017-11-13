2107da4ede5e487811a490224a8af1b5267d83b6
evens64 <kuba.hud@gmail.com>
2017-11-13 18:21:48 +0100

    restrukturalizace

---
+++ TvorbaTestu/js/script.js
@@ -0,0 +1,35 @@
+var id = 0;
+
+//vlozeni otazky
+function addDiv() {
+    var iDiv = document.createElement('div');
+    iDiv.className = 'textarea';
+    iDiv.id = id;
+
+    iDiv.setAttribute("contenteditable", true);
+    iDiv.addEventListener('click', selectElement(id), false);
+    document.getElementById('col-main').appendChild(iDiv);
+    id = id + 1;
+}
+
+function selectElement(id) {
+    var e = document.getElementById('id').event;
+    e.stopPropagation();
+    console.log("clicked");
+}
+
+//nahradit TEXT
+function replaceSelectedText(replacementText) {
+    var sel, range;
+    if (window.getSelection) {
+        sel = window.getSelection();
+        if (sel.rangeCount) {
+            range = sel.getRangeAt(0);
+            range.deleteContents();
+            range.insertNode(document.createTextNode(replacementText));
+        }
+    } else if (document.selection && document.selection.createRange) {
+        range = document.selection.createRange();
+        range.text = replacementText;
+    }
+}
