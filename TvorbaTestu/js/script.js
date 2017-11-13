030f8bb9067506dccf48d403e4b631c6792adbfe
Michal Hrabovský <hrab.m@seznam.cz>
2017-11-13 21:28:17 +0100

    Adding functions for adding various elements of test
    addImage addText addBlankSpace addMultipleChoice.....no styling, no
    shits, looks terrible, smells worse

--- TvorbaTestu/js/script.js
+++ TvorbaTestu/js/script.js
@@ -22,6 +22,70 @@ function addDiv() {
     id = id + 1;
 }
 
+function addImage() {
+    var iDiv = document.createElement('div');
+    var iImage = document.createElement('img');
+    var focus = documument.getElementById(focusId);
+    
+    iDiv.classList.add('testImgDiv');
+    iDiv.classList.add('questionElement');
+    iDiv.id = "testImage_" + id;
+    id++;
+    
+    iImage.src('images\question.png');
+
+    focus.appendChild(iDiv);
+    iDiv.appendChild(iImage);
+}
+
+function addText() {
+    var iDiv = document.createElement('div');
+    var focus = documument.getElementById(focusId);
+
+    iDiv.classList.add('testTextDiv');
+    iDiv.classList.add('questionElement');
+    iDiv.id = "testText_" + id;
+    iDiv.setAttribute("contenteditable", true);
+    id++;
+
+    focus.appendChild(iDiv);
+}
+
+function addBlankSpace() {
+    var iDiv = document.createElement('div');
+    var focus = documument.getElementById(focusId);
+
+    iDiv.classList.add('testBlankDiv');
+    iDiv.classList.add('questionElement');
+    iDiv.id = "testBlankSpace_" + id;
+    id++;
+
+    focus.appendChild(iDiv);
+}
+
+function addMultipleChoice() {
+    var iDiv = document.createElement('div');
+    var focus = documument.getElementById(focusId);
+
+    iDiv.classList.add('testMultipleChoiceDiv');
+    iDiv.classList.add('questionElement');
+    iDiv.id = "testMultipleChoice_" + id;
+    id++;
+
+    for (var i = 0; i < 3 ; i++) {
+        var iSubDiv = document.createElement('div');
+        iSubDiv.classList.add('testMultipleChoiceOptionDiv');
+        iSubDiv.classList.add('questionSubElement');
+        iSubDiv.id = "testMultipleChoice_" + id;
+        
+        iDiv.appendChild(iSubDiv);
+        var text = document.createTextNode("answer");
+        iSubDiv.appendChild(text);
+    }
+
+    focus.appendChild(iDiv);
+}
+
 function selectElement(e) {
     e.stopPropagation();
 
@@ -43,18 +107,18 @@ function selectElement(e) {
     console.log(focusId);
 }
 
-//nahradit TEXT
-function replaceSelectedText(replacementText) {
-    var sel, range;
-    if (window.getSelection) {
-        sel = window.getSelection();
-        if (sel.rangeCount) {
-            range = sel.getRangeAt(0);
-            range.deleteContents();
-            range.insertNode(document.createTextNode(replacementText));
+    //nahradit TEXT
+    function replaceSelectedText(replacementText) {
+        var sel, range;
+        if (window.getSelection) {
+            sel = window.getSelection();
+            if (sel.rangeCount) {
+                range = sel.getRangeAt(0);
+                range.deleteContents();
+                range.insertNode(document.createTextNode(replacementText));
+            }
+        } else if (document.selection && document.selection.createRange) {
+            range = document.selection.createRange();
+            range.text = replacementText;
         }
-    } else if (document.selection && document.selection.createRange) {
-        range = document.selection.createRange();
-        range.text = replacementText;
     }
-}
