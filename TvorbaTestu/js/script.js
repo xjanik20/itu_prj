338dd0e4e516b2bc429ef3d71af080c590b6422f
xjanik20 <luo96@seznam.cz>
2017-12-12 09:45:07 +0100

    final

--- TvorbaTestu/js/script.js
+++ TvorbaTestu/js/script.js
@@ -1,28 +1,96 @@
 var id = 0;
+var focusElement = -1;
 
-//vlozeni otazky
-function addDiv() {
+function addImage() {
     var iDiv = document.createElement('div');
-    iDiv.className = 'textarea';
-    iDiv.id = id;
+    var iImage = document.createElement('img');
 
-    iDiv.setAttribute("contenteditable", true);
-    iDiv.addEventListener('click', selectElement(id, event), false);
+    iDiv.classList.add('testImgDiv');
+    iDiv.id = "testImage-" + id;
+    id++;
 
-    //if (id == 2) {
-        //document.getElementById('1').appendChild(iDiv);
-    //}
-    //else {
-        document.getElementById('col-main').appendChild(iDiv);
-    //}
-    
-    id = id + 1;
+    iImage.src('images\question.png');
+
+    muuriWrapper.addElementTo()
+    iDiv.appendChild(iImage);
 }
 
-function selectElement(id, event) {
-    //var e = document.getElementById('id').event;
-    //event.stopPropagation();
-    console.log("clicked");
+function addText() {
+    var iDiv = document.createElement('div');
+    var focus = documument.getElementById(focusId);
+
+    iDiv.classList.add('testTextDiv');
+    iDiv.classList.add('questionElement');
+    iDiv.id = "testText_" + id;
+    iDiv.setAttribute("contenteditable", true);
+    id++;
+
+    focus.appendChild(iDiv);
+}
+
+function addBlankSpace() {
+    var iDiv = document.createElement('div');
+    var focus = documument.getElementById(focusId);
+
+    iDiv.classList.add('testBlankDiv');
+    iDiv.classList.add('questionElement');
+    iDiv.id = "testBlankSpace_" + id;
+    id++;
+
+    focus.appendChild(iDiv);
+}
+
+function addMultipleChoice() {
+    var iDiv = document.createElement('div');
+    var focus = documument.getElementById(focusId);
+
+    iDiv.classList.add('testMultipleChoiceDiv');
+    iDiv.classList.add('questionElement');
+    iDiv.id = "testMultipleChoice_" + id;
+    id++;
+
+    for (var i = 0; i < 3 ; i++) {
+        var iSubDiv = document.createElement('div');
+        iSubDiv.classList.add('testMultipleChoiceOptionDiv');
+        iSubDiv.classList.add('questionSubElement');
+        iSubDiv.id = "testMultipleChoice_" + id;
+
+        iDiv.appendChild(iSubDiv);
+        var text = document.createTextNode("answer");
+        iSubDiv.appendChild(text);
+    }
+
+    focus.appendChild(iDiv);
+}
+
+function selectElement(e) {
+    e.stopPropagation();
+
+    //console.log(e.type);
+
+    var className = e.target.className;
+    var pattTextArea = new RegExp("textarea");
+    var pattToolImage = new RegExp("tool-image");
+    var pattFocused = new RegExp("focused");
+    var pattQuestionContainer = new RegExp("question-container");
+    var pattAnswersContainer = new RegExp("answers-container");
+
+    if (pattTextArea.test(className) || pattQuestionContainer.test(className) || pattAnswersContainer.test(className)) {
+        focusId = e.target.id;
+        if (!pattFocused.test("focused")) {
+            //e.target.className += ' focused';
+            e.target.classList.add('focused');
+        }
+    }
+    else {
+        if (!pattToolImage.test(className)) {
+            focusId = -1;
+        }
+    }
+
+    console.log("selectElement--focusId: " + focusId);
+
+    //setToolbar(arg)
 }
 
 //nahradit TEXT
@@ -39,4 +107,66 @@ function replaceSelectedText(replacementText) {
         range = document.selection.createRange();
         range.text = replacementText;
     }
+}
+
+
+
+
+// zmena toolbaru podle vybraneho prvku
+function setToolbar(selectedElementType) {
+    //event.stopPropagation();
+
+    switch (selectedElementType) {
+        case "textarea":
+
+            document.getElementById('bold').style.display = 'inline';
+            document.getElementById('italic').style.display = 'inline';
+            document.getElementById('underline').style.display = 'inline';
+            document.getElementById('textColor').style.display = 'inline';
+            document.getElementById('highlight').style.display = 'inline';
+
+            // answer part
+            document.getElementById('deleteAnswer').style.display = 'none';
+            document.getElementById('addImageAnswer').style.display = 'none';
+            break;
+        case "answer":
+            document.getElementById('deleteAnswer').style.display = 'inline';
+            document.getElementById('addImageAnswer').style.display = 'inline';
+
+            // textpart
+            document.getElementById('bold').style.display = 'none';
+            document.getElementById('italic').style.display = 'none';
+            document.getElementById('underline').style.display = 'none';
+            document.getElementById('textColor').style.display = 'none';
+            document.getElementById('highlight').style.display = 'none';
+            break;
+    }
+    return false;
+}
+
+
+//uprava textu
+function editText(char1, char2) {
+    // ziskani textarea
+    var textArea = document.getElementById(focusElement);
+
+    var text = textArea.innerHTML;
+    if ((text.search(char1) === 0) && (text.search(char2) === (text.length - 4)))
+    {
+        text = text.substr(3, (text.length - 4));
+        textArea.innerHTML = text;
+    }
+    else
+        textArea.innerHTML = char1 + text + char2;
+}
+
+
+//změna focusu
+function changeFocus(id) {
+    focusElement = id;
+}
+
+//smazat otázku
+function deleteQuestion(id) {
+    document.getElementById('xxx').remove();
 }
