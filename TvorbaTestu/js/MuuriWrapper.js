6694b4e3c4e2357eb32eb2d34cb2c10326e50034
Michal Hrabovský <hrab.m@seznam.cz>
2017-12-12 02:00:08 +0100

    adding question

--- TvorbaTestu/js/MuuriWrapper.js
+++ TvorbaTestu/js/MuuriWrapper.js
@@ -1,132 +1,137 @@
 var muuriWrapper = {};
 document.addEventListener('DOMContentLoaded', function () {
-    (function(){
-    function MuuriPair(outer, inner) {
-        this.outerElement = outer;
-        this.contentElement = inner;
-    }
-
-    selectedElement = null;
-    var questionMuuriArray = [];
-    var idSequence = 0;
-    var addQuestionButton = document.getElementById("addQuestion");
-
-    var templateElement = document.getElementById("templateArea")
-    var versionElement = document.getElementById("versionArea");
-    var testElement = document.getElementById("questionArea");
-
-    var children = clearElement(templateElement);
-    var templateMuuri = new Muuri(
-        templateElement,
-        {
-            dragSort: templateSort,
-            dragEnabled: true
+    (function () {
+        function MuuriPair(outer, inner) {
+            this.outerElement = outer;
+            this.contentElement = inner;
         }
-    );
-    refillElement(children,templateMuuri);
 
-    children = clearElement(versionElement);
-    var versionMuuri = new Muuri(
-        versionElement,
-        {
-            dragSort:versionSort,
-            dragEnabled: true
-        }
-    );
-    refillElement(children,versionMuuri);
+        selectedElement = null;
+        questionMuuriArray: var questionMuuriArray = [] ;
+        var idSequence = 0;
+        var addQuestionButton = document.getElementById("addQuestion");
 
-    children = clearElement(testElement);
-    var testMuuri = new Muuri(
-        testElement,
-        {
-            dragSort: testSort,
-            dragEnabled: true
-        }
-    );
-    refillElement(children,testMuuri);
+        var templateElement = document.getElementById("templateArea")
+        var versionElement = document.getElementById("versionArea");
+        var testElement = document.getElementById("questionArea");
 
-    function templateSort(item){
-        return [templateMuuri,testMuuri]
-    }
-    function versionSort(item){
-        return [versionMuuri]
-    }
-    function testSort(item){
-        return [templateMuuri,testMuuri]
-    }
-    function Init(){
-        addQuestionButton.addEventListener('click',newQuestion)
-    }
-    /*
-    function createInjectMuuri(element,muuriOptions){
-        var oldchildren = [];
-        for (var i = 0; i < element.childNodes.length;i++){
-            var child = element.removeChild(element.firstChild);
-            oldchildren.push(child);
-        }
-        var muuri = new Muuri(element,muuriOptions)
-        for (var i = 0; i < oldchildren.length;i++){
-            addElementTo(oldchildren[i],muuri);
-        }
-        return muuri;
-    }
-    */
-
-    function clearElement(element){
-        var oldchildren = [];
-        while (element.hasChildNodes()){
-            var child = element.removeChild(element.firstChild);
-            if(child.nodeType === Node.ELEMENT_NODE)
-                oldchildren.push(child);
-        }
-        return oldchildren;
-    }
-    function refillElement(oldchildren,muuri){
-        for (var i = 0; i < oldchildren.length;i++){
-            addElementTo(oldchildren[i],muuri);
-        }
-    }
-
-    function newQuestion() {
-        //creating markup
-        var markup = createQuestionMarkup();
-        testMuuri.add(markup.outerElement);
-        //creating muuri
-        questionMuuriArray.push(
-            new Muuri(
-                markup.contentElement,
-                {
-                    dragEnabled: true,
-                    dragSort: function(item){
-                        return questionMuuriArray
-                    }
-                }
-            )
+        var children = clearElement(templateElement);
+        var templateMuuri = new Muuri(
+            templateElement,
+            {
+                dragSort: templateSort,
+                dragEnabled: true
+            }
         );
-        //addElementTo(createInnerQuestionMarkup(),questionMuuriArray[questionMuuriArray.length - 1]);
-        //adding new question to grif
-        idSequence++;
-        addElementTo(createInnerQuestionMarkup(),questionMuuriArray[questionMuuriArray.length - 1]);
-        return markup;
-    }
+        refillElement(children, templateMuuri);
 
-    function createQuestionMarkup() {
-        //item
-        var itemDiv = document.createElement("div");
-        itemDiv.classList.add('item');
-        //item-content
-        var itemContentDiv = document.createElement("div");
-        itemContentDiv.classList.add('item-content');
-        itemDiv.appendChild(itemContentDiv);
-        //nested grid
-        var gridDiv = document.createElement("div");
-        gridDiv.classList.add('question-grid');
-        gridDiv.id = 'question-grid-' + idSequence;
-        itemContentDiv.appendChild(gridDiv);
-        return new MuuriPair(itemDiv, gridDiv);
-    }
+        children = clearElement(versionElement);
+        var versionMuuri = new Muuri(
+            versionElement,
+            {
+                dragSort: versionSort,
+                dragEnabled: true
+            }
+        );
+        refillElement(children, versionMuuri);
 
-        function createInnerQuestionMarkup(){
+        children = clearElement(testElement);
+        var testMuuri = new Muuri(
+            testElement,
+            {
+                dragSort: testSort,
+                dragEnabled: true
+            }
+        );
+        refillElement(children, testMuuri);
+
+        function templateSort(item) {
+            return [templateMuuri, testMuuri]
+        }
+
+        function versionSort(item) {
+            return [versionMuuri]
+        }
+
+        function testSort(item) {
+            return [templateMuuri, testMuuri]
+        }
+
+        function Init() {
+            addQuestionButton.addEventListener('click', newQuestion)
+        }
+
+        /*
+        function createInjectMuuri(element,muuriOptions){
+            var oldchildren = [];
+            for (var i = 0; i < element.childNodes.length;i++){
+                var child = element.removeChild(element.firstChild);
+                oldchildren.push(child);
+            }
+            var muuri = new Muuri(element,muuriOptions)
+            for (var i = 0; i < oldchildren.length;i++){
+                addElementTo(oldchildren[i],muuri);
+            }
+            return muuri;
+        }
+        */
+
+        function clearElement(element) {
+            var oldchildren = [];
+            while (element.hasChildNodes()) {
+                var child = element.removeChild(element.firstChild);
+                if (child.nodeType === Node.ELEMENT_NODE)
+                    oldchildren.push(child);
+            }
+            return oldchildren;
+        }
+
+        function refillElement(oldchildren, muuri) {
+            for (var i = 0; i < oldchildren.length; i++) {
+                addElementTo(oldchildren[i], muuri);
+            }
+        }
+
+        newQuestion : function newQuestion() {
+            //creating markup
+            var markup = createQuestionMarkup();
+            testMuuri.add(markup.outerElement);
+            //creating muuri
+            questionMuuriArray.push(
+                new Muuri(
+                    markup.contentElement,
+                    {
+                        dragEnabled: true,
+                        dragSort: function (item) {
+                            return questionMuuriArray
+                        }
+                    }
+                )
+            );
+            //addElementTo(createInnerQuestionMarkup(),questionMuuriArray[questionMuuriArray.length - 1]);
+            //adding new question to grif
+            idSequence++;
+            addElementTo(createInnerQuestionMarkup(), questionMuuriArray[questionMuuriArray.length - 1]);
+            return markup;
+        }
+
+        function createQuestionMarkup() {
+            //item
+            var itemDiv = document.createElement("div");
+            itemDiv.classList.add('item');
+            //item-content
+            var itemContentDiv = document.createElement("div");
+            itemContentDiv.classList.add('item-content');
+            itemDiv.appendChild(itemContentDiv);
+            //nested grid
+            var gridDiv = document.createElement("div");
+            gridDiv.classList.add('question-grid');
+            gridDiv.id = 'question-grid-' + idSequence;
+            itemContentDiv.appendChild(gridDiv);
+            return new MuuriPair(itemDiv, gridDiv);
+        }
+
+        function createInnerQuestionMarkup() {
             var text = document.createTextNode("Nová otázka")
             var div = document.createElement("div");
             div.classList.add("question-container");
@@ -135,18 +140,19 @@ document.addEventListener('DOMContentLoaded', function () {
             return div;
         }
 
-    function addElementTo(element, grid) {
-        //item
-        var itemDiv = document.createElement("div");
-        itemDiv.classList.add('item');
-        //inner content
-        var itemContentDiv = document.createElement("div");
-        itemContentDiv.classList.add('item-content');
-        itemDiv.appendChild(itemContentDiv);
-        //adding to grid
-        itemContentDiv.appendChild(element);
-        grid.add(itemDiv);
-    }
-    Init();
+        function addElementTo(element, grid) {
+            //item
+            var itemDiv = document.createElement("div");
+            itemDiv.classList.add('item');
+            //inner content
+            var itemContentDiv = document.createElement("div");
+            itemContentDiv.classList.add('item-content');
+            itemDiv.appendChild(itemContentDiv);
+            //adding to grid
+            itemContentDiv.appendChild(element);
+            grid.add(itemDiv);
+        }
+
+        Init();
     }).apply(muuriWrapper);
-    });
+});
